77d34280878c035f09ce29c4491e8269c81f8a66
madhankmr <51670567+madhankmr@users.noreply.github.com>
2019-06-21 14:50:53 +0530

    Create rev sum.c

---
+++ rev sum.c
@@ -0,0 +1,17 @@
+int main() {
+	int arr[10];
+	int sum=0;
+	int i;
+	printf("enter the 10 elements in an array");
+	for(i=0;i<10;i++)
+	{ 
+	scanf("%d",&arr[i]);
+	}
+	printf("Elements in array");
+	for(i=10;i>0;--i)
+	{
+	 printf("%d",arr[i]);
+	}
+	printf("%d",sum);
+	return 0;
+}
