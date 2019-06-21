f47d73bb9e33df18614f7e1ee8561a20e5316da2
madhankmr <51670567+madhankmr@users.noreply.github.com>
2019-06-21 14:42:56 +0530

    Create arrayrev.c

---
+++ arrayrev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int arr[10];
+	int sum=0;
+	int i;
+	printf("enter the 10 elements in an array");
+	for(i=0;i<10;i++)
+	{ 
+	scanf("%d",&arr[i]);
+	}
+	printf("Elements in array");
+	for(i=10;i>0;--i)
+	{
+	   sum=sum+arr[i];
+	}
+	printf("%d",sum);
+	return 0;
+}
